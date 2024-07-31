54fa2360ebb341b8ca8207470e3c7fd206a15fdf
VerkhoturovIP <verkhoturovip@sibur.ru>
2024-07-31 20:50:57 +0300

    add script #2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import Script from 'next/script';
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Raleway } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/organism/header';
@@ -18,20 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <Script
-                src="https://www.googletagmanager.com/gtag/js?id=G-2XP3SEDVJP"
-                async
-                strategy="lazyOnload"
-            />
-            <Script strategy="lazyOnload">
-                {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-              
-                gtag('config', 'G-2XP3SEDVJP');
-                `}
-            </Script>
+            <GoogleAnalytics gaId="G-2XP3SEDVJP" />
             <body className={raleway.className}>
                 <Header />
                 <main className="container">{children}</main>
